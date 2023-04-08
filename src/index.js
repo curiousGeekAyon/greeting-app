67c0a45c7eaaef97ec2c1d4556901d83cc494146
Ayon Sinha <ayon1234kgp@gmail.com>
2023-04-08 18:44:09 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const authorName="Ayon Sinha";
+const currDate=new Date().toLocaleDateString();
+const currTime=new Date().toLocaleTimeString();
+let time="";
+for(let i=0;i<2;i++)
+   {
+     if(currTime[i]!=':')
+       {
+         time+=currTime[i];
+       }
+   }
+let greeting;
+const helloStyle={
+  color:"yellow",
+  marginRight:"20px"
+}
+const greetingStyle={
+  color:"green"
+}
+const wrapperStyle={
+backgroundColor:"orange",
+margin:"100px auto",
+display:"flex",
+justifyContent:"center",
+width:"50%",
+borderRadius:"20px"
+}
+let hour=currTime.slice(currTime.length-2,currTime.length);
+if(time>='1'&&time<='11'&&hour=="AM")
+   {
+      greeting="Good Morning";
+      greetingStyle.color="green"
+   } 
+else if(time>='12'&&time<='3'&&hour=="PM"){
+      greeting="Good afternoon";
+      greetingStyle.color="yellow"
+}  
+else if(time>='4'&&time<='8'&&hour=="PM")
+      {
+        greeting="Good Evening";
+        greetingStyle.color="Brown"
+      } 
+else{
+        greeting="Good Night";
+        greetingStyle.color="pink"
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div style={wrapperStyle}>
+    <h2 style={helloStyle}>hello sir,👋 </h2>
+    <h2 style={greetingStyle}>{greeting}</h2>
+    </div>
+    {/* <h2>current time is {currTime} </h2> */}
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
